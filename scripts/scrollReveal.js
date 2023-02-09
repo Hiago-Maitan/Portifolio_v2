242ab691c1ead314518c0a576d4f19011f49f850
Maitann <hiagomaitan@gmail.com>
2023-02-09 01:43:40 -0300

    add script Scroll Reveal

---
+++ scripts/scrollReveal.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.about-me-container', { duration: 2000 });
+
+sr.reveal('.about-me-container', {
+    duration: 2500,
+});
+
+sr.reveal('.projects-container', {
+    duration: 2500,
+});
+
+sr.reveal('.skills-container', {
+    duration: 2500,
+});
+
+
+
+
