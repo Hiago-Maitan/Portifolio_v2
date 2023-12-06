04a2336127b445bb7464b16486cd61c5b6e768f9
Maitann <hiagomaitan@gmail.com>
2023-12-06 18:51:37 -0300

    FEAT: update profile infos and projects

--- scripts/viewProjects.js
+++ scripts/viewProjects.js
@@ -1,36 +1,35 @@
 var arrayProjects = [
-    {
-        imgURL: "../assets/imgProjects/devLinks.PNG",
-        webLink: "https://my-dev-links-henna.vercel.app/"
-    },
-    {
-        imgURL: "../assets/imgProjects/formMultistep.png",
-        webLink: "https://form-multistep.vercel.app/"
-    },
-    {
-        imgURL: "../assets/imgProjects/habits.PNG",
-        webLink: "https://hiago-maitan.github.io/Habit_Tracker/"
-    },
-    {
-        imgURL: "../assets/imgProjects/snapclima.PNG",
-        webLink: "https://hiago-maitan.github.io/Snapclima/"
-    },
-    {
-        imgURL: "../assets/imgProjects/tela-login.PNG",
-        webLink: "https://hiago-maitan.github.io/Tela-De-Login/"
-    },
-    {
-        imgURL: "../assets/imgProjects/Calculadora.PNG",
-        webLink: "https://hiago-maitan.github.io/Calculadora/"
-    },
-]
+  {
+    imgURL: "../assets/imgProjects/bikcraft.jpg",
+    webLink: "https://hiago-maitan.github.io/Bikcraft/",
+  },
+  {
+    imgURL: "../assets/imgProjects/tela-login.PNG",
+    webLink: "https://hiago-maitan.github.io/Tela-De-Login/",
+  },
+  {
+    imgURL: "../assets/imgProjects/adacoffee.PNG",
+    webLink: "https://hiago-maitan.github.io/AdaCoffee/",
+  },
+  {
+    imgURL: "../assets/imgProjects/devLinks.PNG",
+    webLink: "https://my-dev-links-henna.vercel.app/",
+  },
+  {
+    imgURL: "../assets/imgProjects/formMultistep.png",
+    webLink: "https://form-multistep.vercel.app/",
+  },
+  {
+    imgURL: "../assets/imgProjects/snapclima.PNG",
+    webLink: "https://hiago-maitan.github.io/Snapclima/",
+  }
+];
 
 // -- Criando as tags HTML no Javascript -- //
 
 for (var indice = 0; indice < arrayProjects.length; indice++) {
-    var projects = document.getElementById("projects")
-    projects.innerHTML += `
+  var projects = document.getElementById("projects");
+  projects.innerHTML += `
     <a href=${arrayProjects[indice].webLink} target="_blank"><img src=${arrayProjects[indice].imgURL} alt="img-project"></a>
-    `
+    `;
 }
-
